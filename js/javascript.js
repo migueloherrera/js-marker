42dc7edb33cc3ab2cc943257c81639aa53783c29
Miguel <miguel.o.herrera@gmail.com>
2016-09-06 18:33:46 -0500

    Add map with marker

--- js/javascript.js
+++ js/javascript.js
@@ -1,23 +1,49 @@
+function addMarker() {
+  var lat = parseFloat(document.getElementById('latitude').value);
+  var lng = parseFloat(document.getElementById('longitude').value);
+  var msg = document.getElementById('message').value;
+  var enteredPlace = {lat: lat, lng: lng};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: enteredPlace,
+    zoom: 8 
+  });
+
+  var marker = new google.maps.Marker({
+    position: enteredPlace,
+    map: map,
+    title: msg
+  });
+  
+  var infowindow = new google.maps.InfoWindow({
+    content: msg
+  });
+  
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+
+}
+
 $(document).ready(function() {
   $('#coordinates').validate({
     submitHandler: function(form) {
-      // call add marker
-      form.submit();
-      return false
-    },
-    invalidHandler: function( event, validator ) {
-      var errors = validator.numberOfInvalids();
-      if (errors) {
-        var message = (errors === 1) ? 'highlighted field' : errors + ' highlighted fields';
-        $('.submitError').show().find('span').html(message);
-      }
+      // call the function to create the map
+      addMarker();
+      // reset the form
+      form.reset();
+      return false;
     },
     rules: {
       latitude: {
-        required: true
+        required: true,
+        max: 90,
+        min: -90
       },
       longitude: {
-        required: true
+        required: true,
+        max: 180,
+        min: -180
       },
       message: {
         required: true
@@ -25,10 +51,14 @@ $(document).ready(function() {
     },
     messages: {
       latitude: {
-        required: 'Please enter a latitude...'
+        required: 'Please enter a latitude...',
+        max: 'The maximum value is 90',
+        min: 'The minimum value is -90'
       },
       longitude: {
-        required: 'Please enter a longitude...'
+        required: 'Please enter a longitude...',
+        max: 'The maximum value is 180',
+        min: 'The minimum value is -180'
       },
       message: {
         required: 'Please enter a message...'
